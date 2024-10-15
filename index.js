dfb223a3c6b1b10c3155ac1d206cb5f7a48d493e
Ben Chong <benchong@bull-b.com>
2024-10-15 16:55:07 +0800

    Update: IDK

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  allowEIO3: true,
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -11,15 +13,19 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
+  setInterval(() => {
+    io.emit("/market/summary", Math.floor(Math.random() * 100));
+    console.log("emit market summary");
+  }, 1000);
   // socket.on("disconnect", () => {
   //   console.log("user disconnected");
   // });
 });
 
-server.listen(3000, () => {
+server.listen(7123, () => {
   console.log("listening on *:3000");
 });
 
